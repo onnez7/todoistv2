3fa74ac7e81a4e2338b5943a4a3e847ca3ea0cd5
Flowdesk <flowdesk@example.com>
2026-04-02 14:28:35 -0400

    feat: initial commit

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -5,7 +5,7 @@ import { Dialog as SheetPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { XIcon } from "lucide-react"
+import { IconX } from "@tabler/icons-react"
 
 function Sheet({ ...props }: React.ComponentProps<typeof SheetPrimitive.Root>) {
   return <SheetPrimitive.Root data-slot="sheet" {...props} />
@@ -75,7 +75,7 @@ function SheetContent({
               className="absolute top-3 right-3"
               size="icon-sm"
             >
-              <XIcon
+              <IconX
               />
               <span className="sr-only">Close</span>
             </Button>
